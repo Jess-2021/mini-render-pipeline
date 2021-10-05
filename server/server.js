4ecedb13ddfb8be29e520ec6591b0b0c2284381c
JararvisQ <286980036@qq.com>
2021-10-05 13:18:18 +0800

    新增->server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,24 @@
-const http = require('http')
-import * as fsPromises from 'fs/promises'
+const Koa = require('koa')
+const path = require('path')
+const fsPromise = require('fs/promises')
+const server = new Koa()
 
-  
+let htmlData;
+fsPromise.readFile(path.resolve(process.cwd(), 'index.html'), {
+  encoding: 'utf8'
+}).then(data => {
+  htmlData = data
+})
+
+server.use(async (ctx, _) => {
+  ctx.response.headers = {
+    'Content-Type': 'text/html',
+    'X-Jar': 'foo',
+  }
+  ctx.status = 200
+  ctx.type = 'text/plain; charset=utf-8'
+  ctx.body = htmlData.toString()
+})
+
+server.listen(8089)
+
